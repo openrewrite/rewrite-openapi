8e5f9e441559d9b78dc7dd7ab821072cc78637ca
SiBorea <siborea.fl@gmail.com>
2024-11-21 19:14:02 +0800

    Handle multiple tags in @Api

---
+++ src/main/java/org/openrewrite/openapi/swagger/MigrateApiToTag.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.openapi.swagger;
+
+import static java.util.Collections.emptyList;
+import org.openrewrite.*;
+import static org.openrewrite.Tree.randomId;
+import org.openrewrite.java.*;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.*;
+import org.openrewrite.marker.Markers;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MigrateApiToTag extends Recipe {
+    private static final String FQN_API = "io.swagger.annotations.Api";
+    private static final String FQN_TAG = "io.swagger.v3.oas.annotations.tags.Tag";
+    private static final String FQN_TAGS = "io.swagger.v3.oas.annotations.tags.Tags";
+    private static final JavaType TYPE_TAG = JavaType.buildType(FQN_TAG);
+
+    @Override
+    public String getDisplayName() {
+        return "Migrate from `@Api` to `@Tag`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Converts `@Api` to `@Tag` annotation and converts the directly mappable attributes and removes the others.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(
+          new UsesType<>(FQN_API, false),
+          new JavaIsoVisitor<ExecutionContext>() {
+              private final AnnotationMatcher apiMatcher = new AnnotationMatcher(FQN_API);
+
+              @Override
+              public J.Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
+                  annotation = super.visitAnnotation(annotation, ctx);
+                  if (apiMatcher.matches(annotation)) {
+                      List<Expression> arguments = annotation.getArguments();
+                      boolean hasTags = arguments
+                        .stream()
+                        .filter(J.Assignment.class::isInstance)
+                        .map(J.Assignment.class::cast).
+                        anyMatch(o -> "tags".equals(o.getVariable().printTrimmed()));
+                      if (hasTags) {
+                          getCursor().putMessageOnFirstEnclosing(J.ClassDeclaration.class, FQN_API, annotation);
+                          // Only remove @Api
+                          annotation = null;
+                      } else {
+                          doAfterVisit(new ChangeAnnotationAttributeName(FQN_API, "value", "name").getVisitor());
+                      }
+
+                      doAfterVisit(new ChangeType(FQN_API, FQN_TAG, true).getVisitor());
+                  }
+                  return annotation;
+              }
+
+              @Override
+              public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+                  classDecl = super.visitClassDeclaration(classDecl, ctx);
+
+                  J.Annotation apiAnnotation = getCursor().getMessage(FQN_API);
+                  if (apiAnnotation != null) {
+                      List<J.Assignment> arguments = apiAnnotation.getArguments().stream()
+                        .filter(J.Assignment.class::isInstance)
+                        .map(J.Assignment.class::cast)
+                        .collect(Collectors.toList());
+
+                      // Get the description from the @Api annotation
+                      String desc = arguments.stream()
+                        .filter(o -> "description".equals(o.getVariable().printTrimmed()))
+                        .findFirst()
+                        .map(assignment -> assignment.getAssignment().printTrimmed())
+                        .orElse(null);
+
+                      // Get the tags from the @Api annotation
+                      String tags = arguments.stream()
+                        .filter(o -> "tags".equals(o.getVariable().printTrimmed()))
+                        .findFirst()
+                        .map(o -> ((J.NewArray) o.getAssignment()).getInitializer().stream()
+                          .map(J::printTrimmed)
+                          .map(t -> "@Tag(name=" + t + (desc != null ? ", description=" + desc : "") + ")")
+                          .collect(Collectors.joining(",\n"))
+                        )
+                        .orElse(null);
+                      if (tags != null) {
+                          List<J.Annotation> origin = classDecl.getLeadingAnnotations();
+                          origin.add(buildTags(tags));
+                          classDecl = classDecl.withLeadingAnnotations(origin);
+
+                          // Force import @Tag
+                          maybeAddImport(FQN_TAG, false);
+                          maybeAddImport(FQN_TAGS);
+                      }
+                  }
+
+                  return classDecl;
+              }
+
+              private J.Annotation buildTags(String tags) {
+                  J.Identifier id = new J.Identifier(randomId(), Space.EMPTY, Markers.EMPTY, emptyList(), "Tags", JavaType.buildType(FQN_TAGS), null);
+                  return new J.Annotation(
+                    randomId(),
+                    Space.EMPTY,
+                    Markers.EMPTY,
+                    id,
+                    JContainer.build(
+                      Space.EMPTY,
+                      Collections.singletonList(
+                        new JRightPadded<>(
+                          new J.Literal(
+                            randomId(),
+                            Space.EMPTY,
+                            Markers.EMPTY,
+                            FQN_TAGS,
+                            "{\n" + tags + "\n}",
+                            null,
+                            JavaType.Primitive.String
+                          ),
+                          Space.EMPTY,
+                          Markers.EMPTY
+                        )
+                      ),
+                      Markers.EMPTY
+                    )
+                  );
+              }
+          }
+        );
+    }
+}
---
+++ src/test/java/org/openrewrite/openapi/swagger/MigrateApiToTagTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.openapi.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import static org.openrewrite.java.Assertions.java;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+public class MigrateApiToTagTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("org.openrewrite.openapi.swagger.SwaggerToOpenAPI")
+          .parser(JavaParser.fromJavaVersion().classpath("swagger-annotations-1.+", "swagger-annotations-2.+"));
+    }
+
+    @DocumentExample
+    @Test
+    void single() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import io.swagger.annotations.Api;
+
+              @Api(value = "Bar")
+              class Example {}
+              """,
+            """
+              import io.swagger.v3.oas.annotations.tags.Tag;
+
+              @Tag(name = "Bar")
+              class Example {}
+              """
+          )
+        );
+    }
+
+    @DocumentExample
+    @Test
+    void multiple() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import io.swagger.annotations.Api;
+
+              @Api(tags={"foo", "bar"}, value = "Ignore", description = "Desc")
+              class Example {}
+              """,
+            """
+              import io.swagger.v3.oas.annotations.tags.Tag;
+              import io.swagger.v3.oas.annotations.tags.Tags;
+
+              @Tags({
+              @Tag(name="foo", description="Desc"),
+              @Tag(name="bar", description="Desc")
+              })
+              class Example {}
+              """
+          )
+        );
+    }
+}
