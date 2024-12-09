55bfd9a9e6eecb28794851eecf74bb815dfd6883
Tim te Beek <tim@moderne.io>
2024-12-09 11:27:37 +0100

    Apply formatter and order imports

--- src/main/java/org/openrewrite/openapi/swagger/MigrateSwaggerDefinitionToOpenAPIDefinition.java
+++ src/main/java/org/openrewrite/openapi/swagger/MigrateSwaggerDefinitionToOpenAPIDefinition.java
@@ -16,8 +16,14 @@
 package org.openrewrite.openapi.swagger;
 
 import org.jspecify.annotations.Nullable;
-import org.openrewrite.*;
-import org.openrewrite.java.*;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
@@ -45,64 +51,64 @@ public class MigrateSwaggerDefinitionToOpenAPIDefinition extends Recipe {
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
         return Preconditions.check(
-          new UsesType<>(FQN_SWAGGER_DEFINITION, false),
-          new JavaIsoVisitor<ExecutionContext>() {
-              private final AnnotationMatcher annotationMatcher = new AnnotationMatcher(FQN_SWAGGER_DEFINITION);
+                new UsesType<>(FQN_SWAGGER_DEFINITION, false),
+                new JavaIsoVisitor<ExecutionContext>() {
+                    private final AnnotationMatcher annotationMatcher = new AnnotationMatcher(FQN_SWAGGER_DEFINITION);
 
-              @Override
-              public J.@Nullable Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
-                  J.Annotation ann = super.visitAnnotation(annotation, ctx);
+                    @Override
+                    public J.@Nullable Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
+                        J.Annotation ann = super.visitAnnotation(annotation, ctx);
 
-                  if (annotationMatcher.matches(ann)) {
-                      Map<String, Expression> args = AnnotationUtils.extractAnnotationArgumentAssignments(ann);
+                        if (annotationMatcher.matches(ann)) {
+                            Map<String, Expression> args = AnnotationUtils.extractAnnotationArgumentAssignments(ann);
 
-                      StringBuilder tpl = new StringBuilder("@OpenAPIDefinition(\n");
-                      List<Expression> tplArgs = new ArrayList<>();
-                      List<String> parts = new ArrayList<>();
+                            StringBuilder tpl = new StringBuilder("@OpenAPIDefinition(\n");
+                            List<Expression> tplArgs = new ArrayList<>();
+                            List<String> parts = new ArrayList<>();
 
-                      Expression basePath = args.get("basePath");
-                      Expression host = args.get("host");
-                      Expression schemes = args.get("schemes");
-                      String servers = "";
-                      if (basePath != null && host != null && schemes != null) {
-                          tpl.append("servers = {\n");
-                          for (Expression scheme : ((J.NewArray) schemes).getInitializer()) {
-                              if (!servers.isEmpty()) {
-                                  servers += ",\n";
-                              }
-                              String schemeName = ((J.FieldAccess) scheme).getSimpleName().toLowerCase();
-                              servers += "@Server(url = \"" + schemeName + "://" + host + basePath + "\")";
-                          }
-                          servers += "\n}";
-                          parts.add(servers);
-                      }
+                            Expression basePath = args.get("basePath");
+                            Expression host = args.get("host");
+                            Expression schemes = args.get("schemes");
+                            String servers = "";
+                            if (basePath != null && host != null && schemes != null) {
+                                tpl.append("servers = {\n");
+                                for (Expression scheme : ((J.NewArray) schemes).getInitializer()) {
+                                    if (!servers.isEmpty()) {
+                                        servers += ",\n";
+                                    }
+                                    String schemeName = ((J.FieldAccess) scheme).getSimpleName().toLowerCase();
+                                    servers += "@Server(url = \"" + schemeName + "://" + host + basePath + "\")";
+                                }
+                                servers += "\n}";
+                                parts.add(servers);
+                            }
 
-                      args.remove("basePath");
-                      args.remove("host");
-                      args.remove("schemes");
-                      args.remove("produces");
-                      args.remove("consumes");
-                      for (Map.Entry<String, Expression> arg : args.entrySet()) {
-                          parts.add(arg.getKey() + " = #{any()}");
-                          tplArgs.add(arg.getValue());
-                      }
-                      tpl.append(String.join(",\n", parts));
-                      tpl.append("\n)");
+                            args.remove("basePath");
+                            args.remove("host");
+                            args.remove("schemes");
+                            args.remove("produces");
+                            args.remove("consumes");
+                            for (Map.Entry<String, Expression> arg : args.entrySet()) {
+                                parts.add(arg.getKey() + " = #{any()}");
+                                tplArgs.add(arg.getValue());
+                            }
+                            tpl.append(String.join(",\n", parts));
+                            tpl.append("\n)");
 
-                      ann = JavaTemplate.builder(tpl.toString())
-                        .imports(FQN_OPENAPI_DEFINITION, FQN_SERVER)
-                        .javaParser(JavaParser.fromJavaVersion().dependsOn(FQN_OPENAPI_DEFINITION, FQN_SERVER))
-                        .build()
-                        .apply(updateCursor(ann), ann.getCoordinates().replace(), tplArgs.toArray());
-                      maybeRemoveImport(FQN_SWAGGER_DEFINITION);
-                      maybeAddImport(FQN_OPENAPI_DEFINITION, false);
-                      maybeAddImport(FQN_SERVER, false);
-                      ann = maybeAutoFormat(annotation, ann, ctx);
-                  }
+                            ann = JavaTemplate.builder(tpl.toString())
+                                    .imports(FQN_OPENAPI_DEFINITION, FQN_SERVER)
+                                    .javaParser(JavaParser.fromJavaVersion().dependsOn(FQN_OPENAPI_DEFINITION, FQN_SERVER))
+                                    .build()
+                                    .apply(updateCursor(ann), ann.getCoordinates().replace(), tplArgs.toArray());
+                            maybeRemoveImport(FQN_SWAGGER_DEFINITION);
+                            maybeAddImport(FQN_OPENAPI_DEFINITION, false);
+                            maybeAddImport(FQN_SERVER, false);
+                            ann = maybeAutoFormat(annotation, ann, ctx);
+                        }
 
-                  return ann;
-              }
-          }
+                        return ann;
+                    }
+                }
         );
     }
 }
